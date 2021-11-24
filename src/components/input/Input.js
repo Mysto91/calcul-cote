859d44f385af06e63003cd6f2e55c735abf63161
Etienne TRAN <etiennetran@hotmail.fr>
2021-11-24 12:06:49 +0100

    Ajout tableau

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -5,11 +5,23 @@ export default class Input extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            quotation_1: 0,
-            quotation_2: 1,
-            nobet_bet1: 0,
-            nobet_bet2: 0,
-            quotationNoBet: 0,
+            betValue: 0,
+            quotationOne: 0,
+            quotationTwo: 1,
+            oneTwoNoBet: {
+                bet1: 0,
+                bet2: 0,
+                quotation: 0,
+                gain: 0,
+                gainNet: 0,
+            },
+            twoOneNoBet: {
+                bet1: 0,
+                bet2: 0,
+                quotation: 0,
+                gain: 0,
+                gainNet: 0,
+            }
         };
     }
 
@@ -17,60 +29,138 @@ export default class Input extends Component {
         const value = event.target.value;
 
         if (isNaN(value)) {
-            console.log("erreur de typage");
             return;
         }
 
         const elementId = event.target.id;
 
-        if (elementId === 'quotation-1') {
-            this.setState({ quotation_1: parseFloat(value) });
-        } else {
-            this.setState({ quotation_2: parseFloat(value) });
+        let state = {};
+
+        switch (elementId) {
+            case 'quotation-1':
+                state = { quotationOne: parseFloat(value) };
+                break;
+            case 'quotation-2':
+                state = { quotationTwo: parseFloat(value) };
+                break;
+            case 'bet':
+                state = { betValue: parseFloat(value) };
+                break;
+            default:
+                break;
         }
 
-        this.updateResult();
+        this.setState(state);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+        const state = this.state;
+
+        if (state.quotationOne !== prevState.quotationOne || state.quotationTwo !== prevState.quotationTwo || state.betValue !== prevState.betValue) {
+            this.setState(
+                {
+                    betValue: !isNaN(state.betValue) ? state.betValue : 1,
+                    quotationOne: !isNaN(state.quotationOne) ? state.quotationOne : 0,
+                    quotationTwo: !isNaN(state.quotationTwo) ? state.quotationTwo : 0,
+                }
+            );
+
+            this.updateResult();
+        }
     }
 
     updateResult = () => {
-        const state = this.state;
 
-        const mise = 10;
+        if (!isNaN(this.state.quotationOne)
+            && !isNaN(this.state.quotationTwo)
+            && !isNaN(this.state.betValue)) {
+            const state = this.state;
 
-        const bet2 = mise / state.quotation_2;
-        const bet1 = mise - bet2;
+            const mise = state.betValue;
+            const quotationOne = state.quotationOne;
+            const quotationTwo = state.quotationTwo;
 
-        this.setState(
-            {
-                nobet_bet1: bet1,
-                nobet_bet2: bet2,
-                quotationNoBet: bet1 * state.quotation_1 / mise,
-            }
-        );
+            this.setState(
+                {
+                    oneTwoNoBet: this.calculateNoBet(mise, quotationOne, quotationTwo),
+                    twoOneNoBet: this.calculateNoBet(mise, quotationTwo, quotationOne)
+                }
+            );
+        }
     }
 
-    render() {
-        /*const state = this.state;
-
-        const mise = 10;
-
-        const bet2 = mise / state.quotation_2;
+    calculateNoBet = (mise, q1, q2) => {
+        const bet2 = mise / q2;
         const bet1 = mise - bet2;
+        const quotation = bet1 * q1 / mise;
 
-        const quotationNoBet = bet1 * state.quotation_1 / mise;*/
+        return {
+            bet1: this.trunc(bet1),
+            bet2: this.trunc(bet2),
+            quotation: this.trunc(quotation),
+            gain: this.trunc(mise * quotation),
+            gainNet: this.trunc(mise * quotation - mise),
+        };
+    }
 
+    /**
+     * 
+     * @param {Number} value 
+     * @param {integer} digit 
+     * 
+     * @return {Number}
+     */
+    trunc = (value, digit = 2) => !isNaN(value) ? value.toFixed(digit) : value;
+
+    render() {
         const state = this.state;
 
+        const tableStyles = {
+            border: '1px solid rgba(0, 0, 0, 0.05)',
+        };
+
         return (
-            <div>
-                <label htmlFor="quotation-1">Cote principale</label>
-                <input id="quotation-1" type="text" onChange={this.handleChange} />
-                <label htmlFor="quotation-2">Cote secondaire</label>
-                <input id="quotation-2" type="text" onChange={this.handleChange} />
-                <div id="no-bet">Cote remboursée : {state.quotationNoBet.toFixed(2)}</div>
-                <div id="no-bet">Mise principale {state.nobet_bet1.toFixed(2)}</div>
-                <div id="no-bet">Mise secondaire {state.nobet_bet2.toFixed(2)}</div>
-            </div>
+            <div className="vertical-center">
+                <form>
+                    <label htmlFor="bet">Mise</label>
+                    <input id="bet" type="text" onChange={this.handleChange} />
+                    <label htmlFor="quotation-1">Cote principale</label>
+                    <input id="quotation-1" type="text" onChange={this.handleChange} />
+                    <label htmlFor="quotation-2">Cote secondaire</label>
+                    <input id="quotation-2" type="text" onChange={this.handleChange} />
+                </form>
+                <table style={tableStyles}>
+                    <thead>
+                        <tr>
+                            <th colSpan="1">Pari</th>
+                            <th colSpan="1">Cote</th>
+                            <th colSpan="1">Mise principale</th>
+                            <th colSpan="1">Mise secondaire</th>
+                            <th colSpan="1">Gain</th>
+                            <th colSpan="1">Gain net</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1 remboursé si 2</td>
+                            <td>{state.oneTwoNoBet.quotation}</td>
+                            <td>{state.oneTwoNoBet.bet1} €</td>
+                            <td>{state.oneTwoNoBet.bet2} €</td>
+                            <td>{state.oneTwoNoBet.gain} €</td>
+                            <td>{state.oneTwoNoBet.gainNet} €</td>
+                        </tr>
+                        <tr>
+                            <td>2 remboursé si 1</td>
+                            <td>{state.twoOneNoBet.quotation}</td>
+                            <td>{state.twoOneNoBet.bet1} €</td>
+                            <td>{state.twoOneNoBet.bet2} €</td>
+                            <td>{state.twoOneNoBet.gain} €</td>
+                            <td>{state.twoOneNoBet.gainNet} €</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div >
         )
     }
 }
