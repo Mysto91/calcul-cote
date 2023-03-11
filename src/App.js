9b1ac3b267c22483fcb780163bb5f1499566c8fb
Etienne TRAN <etiennetran@hotmail.fr>
2023-03-11 21:25:54 +0100

    unzoom_mobile : dev

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
 import Input from "./components/input/Input";
 
+function handleOnClick(elementId) {
+	const inputIds = ['quotation-1', 'quotation-2', 'bet'];
+
+	if (!inputIds.includes(elementId)) {
+		const viewport = document.querySelector('meta[name=viewport]');
+		viewport.content = 'width=device-width, initial-scale=1';
+	}
+}
+
 function App() {
 	return (
-		<div className="App">
+		<div className="App" onClick={(event) => handleOnClick(event.target.id)}>
 			<Input />
 		</div>
 	);
